c2881e29937e4abc9eb744936f935989311ad249
Abdulazeez_A <97477565+shy-programmer@users.noreply.github.com>
2025-12-01 20:15:50 +0100

    feat/updating route files

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const dotenv = require('dotenv').config();
 const { connect } = require('./config/database');
 connect();
 const PORT = process.env.PORT || 3000;
+const userRoutes = require('./routes/user.route');
+const gameSessionRoutes = require('./routes/game_session.route');
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
+app.use('/users', userRoutes);
+app.use('/game-sessions', gameSessionRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,4 +1,12 @@
 const userController = require('../controllers/user.controller');
+const {loginValidator, signupValidator} = require('../validators/user.validator');
 const express = require('express');
 const router = express.Router();
 
+router.post('/login', loginValidator, userController.login);
+router.post('/signup', signupValidator, userController.signup);
+router.get('/profile/:userId', userController.getProfile);
+router.put('/profile/:userId', userController.updateProfile);
+router.delete('/profile/:userId', userController.deleteUser);
+
+module.exports = router;
